664a16e27044b66bafafe2345e1c958ee5067bb6
hansenjl <jenn.leigh.hansen@gmail.com>
2017-07-19 14:36:57 -0700

    Defined list_categories for the CLI class and started the user interface

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,13 +2,21 @@ require_relative '../config/environment.rb'
 class CLI
 
   def call
-    create_categories
+    puts "Welcome to the Serious Eats Recipe Finder!"
+    puts "What type of recipe are you looking for today?"
+    list_categories
+
   end
 
   def create_categories
     categories = Scraper.list_categories("http://www.seriouseats.com/recipes")
     categories.each{|c|
       Category.new(c.to_s)}
+  end
+
+  def list_categories
+    create_categories.each_with_index{|c,idx|
+      puts "#{idx+1}. #{c}" }
   end
 
 
