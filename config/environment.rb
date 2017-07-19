10982222968b2ae922e86cfebf5989eec2ac1b67
hansenjl <jenn.leigh.hansen@gmail.com>
2017-07-19 11:48:18 -0700

    require food.rb in environment

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,7 @@ require ‘bundle’ #Load bundler
 Bundler.require # Require the Gems from Gemfile using bundler
 
 #Load Libraries
-require_relative '..lib/pizza.rb'
-require_relative '..lib/chicken.rb'
+require_relative '..lib/food_item.rb'
 require_relative '..lib/categories.rb'
 require_relative '..lib/scraper.rb'
 require_relative '../lib.command_line_interface.rb'
